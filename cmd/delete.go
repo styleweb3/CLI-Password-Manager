db178fa03fc7bf8dd38091f6c14261d2d366696b
Sahaidak Yaroslav <sa2ya0809@gmail.com>
2026-09-01 00:21:31 +0300

    Add delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,2 +1,54 @@
 package cmd
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/styleweb3/CLI-pwd-manager/internal/store"
+	"github.com/styleweb3/CLI-pwd-manager/models"
+)
+
+var deleteCMD = &cobra.Command{
+	Use: 	"del [service]",
+	Short: 	"Delete a password entry by service name",
+	Args: 	cobra.ExactArgs(1),
+	Run: 	func(cmd *cobra.Command, args []string){
+				searchTerm := strings.ToLower(args[0])
+
+        		entries, err := store.Load()
+        		
+				if err != nil {
+            		fmt.Println("Error loading vault:", err)
+            		return
+      			}
+
+				var filtered []models.PasswordEntry
+				found := false
+				
+				for _, entry := range entries{
+					if strings.ToLower(entry.ServiceName) == searchTerm {
+                		found = true
+                		continue
+            		}
+            		filtered = append(filtered, entry)
+				}
+
+				if !found {
+					fmt.Printf("No entry found for: %s\n", args[0])
+					return
+				}
+
+				err = store.Save(filtered)
+        		if err != nil {
+          			fmt.Println("Error saving vault:", err)
+            		return
+        		}
+
+       			fmt.Printf("• Entry deleted for: %s\n", args[0])
+			},
+}
+
+func init(){
+	rootCmd.AddCommand(deleteCMD)
+}
--- cmd/list.go
+++ cmd/list.go
@@ -26,7 +26,7 @@ var listCMD = &cobra.Command{
 			fmt.Println("-----------------------------")
 			fmt.Printf("• Service:    %s", entry.ServiceName)
 			fmt.Printf("\n• Log-In:      %s", entry.LogIn)
-			fmt.Printf("\n• Created At: %s", entry.CreatedAt.Format("2006-01-02 15:04"))
+			fmt.Printf("\n• Created At: %s\n", entry.CreatedAt.Format("2006-01-02 15:04"))
 		}
 	
 			fmt.Println("-----------------------------")
